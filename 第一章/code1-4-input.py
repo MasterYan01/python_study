bfe2ce9509b549d61599ecdca60e9d9343c7f32e
Ethan <ethanyan@castec.com.tw>
2024-06-27 15:51:22 +0800

    從P15開始看

---
+++ 第一章/code1-4-input.py
@@ -0,0 +1,13 @@
+name = input("請輸入你的名字:")
+# print(name)
+year = 2024
+# print(type(name))
+age = input("請輸入你的年齡:")
+g = int(age)
+birth = year - g
+# print(type(g))
+print("你的出生年月日",birth)
+
+
+#從P15開始看
+# https://www.bilibili.com/video/BV1eZ421b7ag/?p=15&spm_id_from=pageDriver&vd_source=6d571e7c74d5b7175d1e776ab2bc8922

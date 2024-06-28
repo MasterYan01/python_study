a1ed6015d5e2d2a4f3a880d070671a48fc23ba1b
Ethan <ethanyan@castec.com.tw>
2024-06-28 15:15:40 +0800

    python基礎學習

---
+++ 第三章/code3-1-算數運算符.py
@@ -0,0 +1,15 @@
+a = 10
+b = 3
+# print(a / b)
+# print(a // b)
+# print(a % b)
+# print(a ** 9)
+# print('盧' == '盧')
+# print(ord('盧'))
+# print(5>>2)
+e = 'abc'
+print(e.upper())
+
+
+#從第P37開始看
+# https://www.bilibili.com/video/BV1eZ421b7ag?p=37&spm_id_from=pageDriver&vd_source=6d571e7c74d5b7175d1e776ab2bc8922
---
+++ 第二章/code2-1-創建變量.py
@@ -0,0 +1,52 @@
+# price = 10.5 
+# weight = 7.5
+# money = price * weight
+# print("總共消費了%.2f元"% money )
+
+# money = 123.456
+# print(f"總共消費了{money:.2f}元")
+
+# year = 2023 
+# print(year)
+# year = 2024
+# print(year)
+
+# a = '1234asd'
+# print(type(a))
+# print(isinstance(a,str))
+
+# 四捨五入 round
+n1 = 0.1
+n2 = 0.2
+n3 = round(n1+n2,2)
+# print(n3)
+
+import math
+#向上取整 ceil
+n4 = math.ceil(n1+n2)
+# print(n4,'n4')
+str3 = '''hello python'''
+str4 = '1234\'\"666' # \\是轉義符
+# print(str3,'str3')
+# print(str4,'str4')
+# print(str3 * 5)
+# print('$' * 20)
+
+s = 'hello python'
+s1 = '123456789'
+# print(s[0:3]) #包頭部包尾
+# print(s[0:3:1]) 
+#字符串反轉
+# print(s1[-1:-10:-2]) 
+# print(s1[::-2]) 
+s = '123abc'
+s1 = ''
+# print(bool(s1))
+
+qwe1 = 300
+qwe2 = 300
+print('*' *20)
+print(id(qwe1))
+print(id(qwe2))
+
+
